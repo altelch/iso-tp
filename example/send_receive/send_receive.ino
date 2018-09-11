3ee81b8dbeaf3f22a3030c0312091322e7794ec7
Heiko Krupp <altelch@users.noreply.github.com>
2018-09-11 17:37:28 +0200

    Fix calloc

--- example/send_receive/send_receive.ino
+++ example/send_receive/send_receive.ino
@@ -14,6 +14,9 @@ uint8_t mf_test[] = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09
                       0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F };
 uint32_t can_id = 0x7E0;
 
+TxMsg.Buffer=(uint8_t *)calloc(MAX_MSGBUF,sizeof(uint8_t));
+RxMsg.Buffer=(uint8_t *)calloc(MAX_MSGBUF,sizeof(uint8_t));
+
 void setup()
 {
   Serial.begin(1000000);
@@ -25,7 +28,6 @@ void setup()
 void loop()
 { 
   TxMsg.len=sizeof(sf_test);
-  TxMsg.Buffer=(uint8_t *)calloc(MAX_MSGBUF,sizeof(uint8_t));
   TxMsg.tx_id=can_id;
   TxMsg.rx_id=can_id+0x20;
   memcpy(TxMsg.Buffer,sf_test,sizeof(sf_test));
@@ -34,7 +36,6 @@ void loop()
   
   RxMsg.tx_id=can_id;
   RxMsg.rx_id=can_id+0x20;
-  RxMsg.Buffer=(uint8_t *)calloc(MAX_MSGBUF,sizeof(uint8_t));
   Serial.println(F("Receive..."));
   isotp.receive(&RxMsg);
 	isotp.print_buffer(RxMsg.rx_id, RxMsg.Buffer, RxMsg.len);
