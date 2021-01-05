c1e378f0e6594fb9964dd17a69686cf1aeecfdf3
Brandon Ros <brandonros1@gmail.com>
2021-01-05 00:24:33 -0500

    INT32U debacle

--- iso-tp.h
+++ iso-tp.h
@@ -36,7 +36,7 @@ typedef enum {
 #define TIMEOUT_SESSION  500 /* Timeout between successfull send and receive */
 #define TIMEOUT_FC       250 /* Timeout between FF and FC or Block CF and FC */
 #define TIMEOUT_CF       250 /* Timeout between CFs                          */
-#define MAX_FCWAIT_FRAME  10   
+#define MAX_FCWAIT_FRAME  10
 
 #define MAX_MSGBUF 128    /* Received Message Buffer. Depends on uC ressources!
                              Should be enough for our needs */
@@ -48,40 +48,40 @@ struct Message_t
   uint8_t fc_status=ISOTP_FC_CTS;
   uint8_t blocksize=0;
   uint8_t min_sep_time=0;
-  uint32_t tx_id=0;
-  uint32_t rx_id=0;
+  INT32U tx_id=0;
+  INT32U rx_id=0;
   uint8_t *Buffer;
 };
 
 class IsoTp
 {
-	public:
-		IsoTp(MCP_CAN* bus, uint8_t mcp_int);
-		uint8_t send(Message_t* msg);
-		uint8_t receive(Message_t* msg);
-		void    print_buffer(uint32_t id, uint8_t *buffer, uint16_t len);
-	private:
-		MCP_CAN* _bus;
-                uint8_t  _mcp_int;
-		uint32_t rxId;
-		uint8_t  rxLen;
-		uint8_t  rxBuffer[8];
-                uint16_t rest;
-		uint8_t  fc_wait_frames=0;
-		uint32_t wait_fc=0;
-		uint32_t wait_cf=0;
-                uint32_t wait_session=0;
-		uint8_t  can_send(uint32_t id, uint8_t len, uint8_t *data);
-		uint8_t  can_receive(void);
-		uint8_t  send_fc(struct Message_t* msg);
-		uint8_t  send_sf(struct Message_t* msg);
-		uint8_t  send_ff(struct Message_t* msg);
-		uint8_t  send_cf(struct Message_t* msg);
-		uint8_t  rcv_sf(struct Message_t* msg);
-		uint8_t  rcv_ff(struct Message_t* msg);
-		uint8_t  rcv_cf(struct Message_t* msg);
-		uint8_t  rcv_fc(struct Message_t* msg);
+  public:
+    IsoTp(MCP_CAN* bus, uint8_t mcp_int);
+    uint8_t send(Message_t* msg);
+    uint8_t receive(Message_t* msg);
+    void    print_buffer(INT32U id, uint8_t *buffer, uint16_t len);
+  private:
+    MCP_CAN* _bus;
+    uint8_t  _mcp_int;
+    INT32U   rxId;
+    uint8_t  rxLen;
+    uint8_t  rxBuffer[8];
+    uint16_t rest;
+    uint8_t  fc_wait_frames=0;
+    INT32U   wait_fc=0;
+    INT32U   wait_cf=0;
+    INT32U   wait_session=0;
+    uint8_t  can_send(INT32U id, uint8_t len, uint8_t *data);
+    uint8_t  can_receive(void);
+    uint8_t  send_fc(struct Message_t* msg);
+    uint8_t  send_sf(struct Message_t* msg);
+    uint8_t  send_ff(struct Message_t* msg);
+    uint8_t  send_cf(struct Message_t* msg);
+    uint8_t  rcv_sf(struct Message_t* msg);
+    uint8_t  rcv_ff(struct Message_t* msg);
+    uint8_t  rcv_cf(struct Message_t* msg);
+    uint8_t  rcv_fc(struct Message_t* msg);
     void     fc_delay(uint8_t sep_time);
 };
-                             
+
 #endif
